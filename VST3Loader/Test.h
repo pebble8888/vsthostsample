b3caaa2f6256a6d38bf6b3b3d4d712015dfb7702
pebble8888 <pebble8888@gmail.com>
2024-07-14 23:44:47 +0900

    add VST3 source

---
+++ VST3Loader/Test.h
@@ -0,0 +1,17 @@
+//
+//  Test.h
+//  AUv3Host macOS
+//
+//  Created by pebble8888 on 2024/07/14.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
